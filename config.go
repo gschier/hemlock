226b321f5d079baf9de03015c4288744cad2d37b
Spencer Dixon <spencercdixon@gmail.com>
2018-01-26 13:28:59 -0500

    Adds comments to improve go-lint score for go report card

--- config.go
+++ config.go
@@ -1,5 +1,8 @@
 package hemlock
 
+// Config contains the static configuration for a Hemlock application.  There is
+// an optional 'Extra' field for storing configuration not directly related to
+// the core Hemlock functionality.
 type Config struct {
 	Name               string
 	Env                string
@@ -12,17 +15,20 @@ type Config struct {
 	Extra              []interface{}
 }
 
+// HTTPConfig contains server configuration.
 type HTTPConfig struct {
 	Host string
 	Port string
 }
 
+// DatabaseConfig contains database settings.
 type DatabaseConfig struct {
 	Default     string // 'postgres'
 	Migrations  string // 'migrations'
 	Connections []DatabaseConnectionConfig
 }
 
+// DatabaseConnectionConfig contains settings for connecting to DB instances.
 type DatabaseConnectionConfig struct {
 	Driver    string
 	Host      string
